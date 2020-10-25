56874eb9542cbb087bc49aea2846d2195929cac4
vincevince1006 <vincevince1006@gmail.com>
2020-10-25 16:59:47 -0400

    color updates, mobile styles, new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,37 +13,40 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="Home" />
       <section class="homepageIntro">
-        <h2>I'm a UI/UX Developer</h2>
+        <Image className="homepageIntro__img" fixed={data.avatar.childImageSharp.fixed} />
         <div class="homepageIntro__inner">
+        <h2>I'm a UI/UX Developer</h2>
           <h3>
             For the past few years I've worked in the civic tech sector, collaborating with other designers and engineers to make web sites and applications  easier to use.
           </h3>
-          <p>I'm passionate about diving into both UX design and crafting polished frontend code, blending hard-won knowledge from both disciplines. Check out my <a href="/works">portfolio</a> and <a href="/resume">resume</a> to learn more.</p>
+          <p><strong><a href="/works">View my portfolio &rarr;</a></strong></p>
         </div>
       </section>
+      <section class="homepageWriting">
       <h2>Writing</h2>
       <p>Tips, tricks, and other things I've found helpful as a developer/designer.</p>
-      <section className="main__inner-articleList">
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-              <h3>
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small><strong>{node.frontmatter.date}</strong></small>
-            <div className="article__preview">
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          </article>
-        )
-      })}
+        <section className="main__inner-articleList">
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+                <h3>
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small><strong>{node.frontmatter.date}</strong></small>
+              <div className="article__preview">
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            </article>
+          )
+        })}
+        </section>
       </section>
     </Layout>
   )
@@ -52,6 +56,17 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query {
+    avatar: file(absolutePath: { regex: "/bio.jpg/" }) {
+      childImageSharp {
+        fixed(
+          quality: 100,
+          width: 150,
+          height: 150
+        ) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     site {
       siteMetadata {
         title
