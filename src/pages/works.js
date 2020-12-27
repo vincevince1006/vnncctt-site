31163b312f30a3a7fcbcbf575a114a511f96afe5
vincevince1006 <vincevince1006@gmail.com>
2020-12-27 17:56:16 -0500

    updates portfolio page

--- src/pages/works.js
+++ src/pages/works.js
@@ -10,82 +10,137 @@ const PortfolioIndex = ({ data }) => {
 
   return (
     <Layout title={siteTitle}>
+
     <SEO title="Portfolio" />
-      <div className="fancy-gradient">
-        <section className="base-container">
-        <h2>Portfolio</h2>
-        <p>A small sampling of projects I've worked on.</p>
-        </section>
-      </div>
-      <div className="base-container portfolioBody">
-      <section className="portfolioBody__project">
-        <h3>govinfo.gov: main document repository</h3>
 
-        <small><strong>Role:</strong> User Interface Designer | 2019 - present | <a href="https://www.govinfo.gov/" target="_blank" rel="noopener noreferrer">https://www.govinfo.gov/</a></small>
-
-        <Img className="portfolioImg" fluid={data.govinfo.childImageSharp.fluid} alt="" />
-
-        <p>govinfo.gov is the official document repository for publications from Congress and various other government branches. As the sole designer on this contract, I manage the front end of a large Backbone.js application and Drupal 7 website that was built in 2016. The biggest challenge has been ensuring that new designs and code are uniform between the two environments. The project has taught me about the importance of creating design systems, digital governance, and how to build functional, reusable design components within a Javascript ecosystem.</p>
-        <h4>Selected Accomplishments</h4>
-        <ul>
-          <li>Creating reusable design components shared between a CMS and a web application</li>
-          <li>Prototyping designs and implementing them into full working products</li>
-          <li>Developed a visual regression testing tool using a Javascript testing framework</li>
-          <li>Making improvements to existing designs based on client feedback</li>
-        </ul>
+    <div className="fancy-gradient">
+      <section className="base-container">
+      <h2>Portfolio</h2>
+      <p>A small sampling of projects I've worked on.</p>
       </section>
+    </div>
 
-      <section className="portfolioBody__project">
-        <h3>govinfo.gov: curated content search</h3>
+    <div className="base-container portfolioBody">
+    <section className="portfolioBody__project">
+      <h3>govinfo.gov: main document repository</h3>
 
-        <small><strong>Role:</strong> User Interface Designer | 2019 - present | <a href="https://www.govinfo.gov/cs/" target="_blank" rel="noopener noreferrer">https://www.govinfo.gov/cs/</a></small>
+      <small><strong>Role:</strong> User Interface Designer | 2019 - present | <a href="https://www.govinfo.gov/" target="_blank" rel="noopener noreferrer">https://www.govinfo.gov/</a></small>
 
-        <Img className="portfolioImg" fluid={data.govinfo_cs.childImageSharp.fluid} alt="" />
+      <Img className="portfolioImg" fluid={data.govinfo.childImageSharp.fluid} alt="" />
 
-        <p>govinfo.gov allows users to search across government documents, but there's also curated content in the form of articles, help documentation for end users, and developer documentation that weren't previously searchable. I created a working prototype for federal clients to test and critique, and then developed the front-end with React.js components. This introduced me to designing/architecting components within React.js, how to interact with a complex backend environment, and will inform design/front-end choices for future React applications.</p>
-        <h4>Selected Accomplishments</h4>
-        <ul>
-          <li>Developed reusable design patterns and React.js components that interact with a custom search API</li>
-          <li>Created UI mockups to visualize user process for search experience</li>
-          <li>Converted older Javascript/jQuery functionality into modern React.js code</li>
-          <li>Quickly learned new software design principles and launched product within a strict deadline</li>
-        </ul>
-      </section>
+      <h4>Overview</h4>
+      <p>govinfo.gov is the official document repository for publications from Congress and various other government branches. I'm the project's sole designer, managing both the UI design and front end code for various sites and applications.</p>
 
-      <section className="portfolioBody__project">
-        <h3>HCPSS StoryStrong</h3>
+      <h4>Primary Duties</h4>
+      <ul>
+        <li>Gathering design requirements and ideas from internal federal clients</li>
+        <li>Creating mockups, user flows, and high-fidelity prototypes to guide design choices</li>
+        <li>Developing the front end for new UI features using HTML, SCSS, and Javascript</li>
+      </ul>
 
-        <small><strong>Role:</strong> Front-end Developer | 2017 | <a href="https://storystrong.hcpss.org/" target="_blank" rel="noopener noreferrer">https://storystrong.hcpss.org/</a></small>
+      <h4>Dev Tools:</h4>
+      <p>React.js, Drupal, Backbone.js, Gulp/Webpack</p>
 
-        <Img className="portfolioImg" fluid={data.storystrong.childImageSharp.fluid} alt="" />
+      <h4>Design Tools:</h4>
+      <p>Adobe XD, Photoshop, Illustrator, SCSS, Bootstrap, Fractal.js</p>
 
-        <p>During my time at Howard County Public School System, I was approached by leadership staff who wanted to build a platform for students and staff to post blog entries using text, video, and audio.</p>
-        <p>I conducted interviews with the project leaders to create content types, establish user submission requirements, and mocked up what each content type would look like using Adobe Experience Designer (XD). From there I took the completed mockups and built a site from the ground up using Jekyll, a Ruby-based static site generator, and worked with staff to upload/maintain content. </p>
-        <h4>Selected Accomplishments</h4>
-        <ul>
-          <li>Took this from content strategy to UI prototyping, and built the full working product</li>
-          <li>Collaborated with cross-departmental staff to decide which technologies to use, strategize ways to gather/manage content, and recieve feedback on designs</li>
-          <li>Used a mobile-first responsive design approach and modern CSS/design methodologies. Successes with these new methodologies informed future web products</li>
-          <li>Helped create a content publishing schedule and requirements for new site content</li>
-        </ul>
-      </section>
+      <h4>Selected Accomplishments</h4>
+      <ul>
+        <li>Built reusable UI design components shared between a CMS and multiple web applications</li>
+        <li>Created a living style guide to document both existing and new design choices</li>
+        <li>Improved code for site social media sharing functionality and search metadata creation</li>
+        <li>Developed a visual regression testing tool using a Javascript testing framework</li>
+      </ul>
+    </section>
 
-      <section className="portfolioBody__project">
-        <h3>HCPSS Staff Directory</h3>
+    <section className="portfolioBody__project">
+      <h3>govinfo.gov: curated content search</h3>
 
-        <small><strong>Role:</strong> Front-end Developer | 2017-2018 | <a href="https://directory.hcpss.org/" target="_blank" rel="noopener noreferrer">https://directory.hcpss.org/</a></small>
+      <small><strong>Role:</strong> User Interface Designer | 2019 - present | <a href="https://www.govinfo.gov/cs/" target="_blank" rel="noopener noreferrer">https://www.govinfo.gov/cs/</a></small>
 
-        <Img className="portfolioImg" fluid={data.directoryProject.childImageSharp.fluid} alt="" />
+      <Img className="portfolioImg" fluid={data.govinfo_cs.childImageSharp.fluid} alt="" />
 
-        <p>Howard County Public School System was using a large PDF file as it’s online phone directory, which was maintained by a single staff member not associated with Human Resources. HCPSS had moved to Workday as it’s HR system and I saw an opportunity to create a dynamic and searchable directory. I collaborated with data analysts from HR to strategize the best ways to get employee data and maintain it within the web application.</p>
-        <p>I chose to build it using Symfony2, a PHP MVC framework, because we had a subject matter expert I could look to for advice. This application was the first of it’s kind for the school system, and continues to deliver up to date contact information for staff and the public.</p>
-        <h4>Selected Accomplishments</h4>
-        <ul>
-          <li>Built both the front and back end of a complex application from the ground up</li>
-          <li>Decided on functionality and design while weighing technical decisions on application architecture, the employee data to display, and user experience</li>
-          <li>Took unorganized data and turned it into useful, structured data for web use</li>
-        </ul>
-      </section>
+      <p>govinfo.gov primary function is to search across government documents, but there's also curated content in the form of feature articles, help documentation for end users, and developer documentation that needed to be made searchable and have a corresponding UI.</p>
+
+      <h4>Primary Duties</h4>
+      <ul>
+        <li>Create mockups to model what the search experience would look like for end users</li>
+        <li>Write React.js components and SCSS based on approved designs alongside a team of engineers</li>
+        <li>Ensure all front end code and UI styles is consistent with the main govinfo project</li>
+      </ul>
+
+      <h4>Dev Tools:</h4>
+      <p>React.js, Drupal, Webpack</p>
+
+      <h4>Design Tools:</h4>
+      <p>Adobe XD, Photoshop, SCSS, Bootstrap</p>
+
+      <h4>Selected Accomplishments</h4>
+      <ul>
+        <li>Brought design ideas from concepts to a full working product in React.js</li>
+        <li>Rewrote all front end components from jQuery to a React.js/ES6 architecture</li>
+        <li>Made Drupal webpage metadata and HTML structures more search-engine friendly</li>
+      </ul>
+
+    </section>
+
+    <section className="portfolioBody__project">
+      <h3>HCPSS StoryStrong</h3>
+
+      <small><strong>Role:</strong> Front-end Developer | 2017 | <a href="https://storystrong.hcpss.org/" target="_blank" rel="noopener noreferrer">https://storystrong.hcpss.org/</a></small>
+
+      <Img className="portfolioImg" fluid={data.storystrong.childImageSharp.fluid} alt="" />
+
+      <p>During my time at HCPSS, I was approached by leadership staff who wanted to build a self-hosted platform for students and staff to post blog entries using text, video, and audio. This was the first of it's kind for the organization and had to be turned around on a tight deadline for a community event showcasing the entires.</p>
+
+      <h4>Primary Duties</h4>
+      <ul>
+        <li>Conduct interviews with project leaders to create content types, wireframes, and establish user submission requirements</li>
+        <li>Design/code the UI for a static site generator (Jekyll) and create standardized templates for all content</li>
+      </ul>
+
+      <h4>Dev Tools:</h4>
+      <p>Jekyll (a Ruby-based static site generator), Gulp</p>
+
+      <h4>Design Tools:</h4>
+      <p>Adobe XD, Photoshop, SCSS</p>
+
+      <h4>Selected Accomplishments</h4>
+      <ul>
+        <li>Took this from content strategy to UI prototyping, and built the full working product</li>
+        <li>Used a mobile-first responsive design approach and modern CSS/design methodologies, successes with these new methodologies informed future web products</li>
+        <li>Helped create a content publishing schedule and requirements for new site content</li>
+      </ul>
+    </section>
+
+    <section className="portfolioBody__project">
+      <h3>HCPSS Staff Directory</h3>
+
+      <small><strong>Role:</strong> Front-end Developer | 2017-2018 | <a href="https://directory.hcpss.org/" target="_blank" rel="noopener noreferrer">https://directory.hcpss.org/</a></small>
+
+      <Img className="portfolioImg" fluid={data.directoryProject.childImageSharp.fluid} alt="" />
+
+      <p>HCPSS had moved to Workday as it’s HR system and it provided up to date contact information for all staff into one place. This was an opportunity to create a dynamic and searchable directory to replace their former PDF-centric directory that was not user friendly.</p>
+
+      <h4>Primary Duties</h4>
+      <ul>
+        <li>Work with HR data analysts to strategize ways to programatically fetch employee contact information and maintain it within a web application</li>
+        <li>Build both UI and back end functionality to consume data and provide an interface to search for active employees</li>
+      </ul>
+
+      <h4>Dev Tools:</h4>
+      <p>Symfony2, PHP/MySQL, Twig</p>
+
+      <h4>Design Tools:</h4>
+      <p>SCSS</p>
+
+      <h4>Selected Accomplishments</h4>
+      <ul>
+        <li>Created a full project plan including how data would be synced from an external system, database design/application flow, and data quality policies</li>
+        <li>Decided on functionality and design while weighing technical decisions on application architecture, employee contact information to display, and user experience</li>
+        <li>Took unorganized data sets and turned it into useful, structured data for web use</li>
+      </ul>
+    </section>
     </div>
     </Layout>
   )
